b32e50972b8a1affdba5988daf01729bc528552e
JianboLi-github <1060012085@qq.com>
2020-04-13 17:31:15 +0800

    object exercise

---
+++ Java/workspace/TheRoadToTheGarden/Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    String name;
+    int price;
+    
+    public static void main(final String[] args) {
+      final Item pot = new Item();
+      pot.name = "血瓶";
+      pot.price = 50;
+
+      final Item shoe = new Item();
+      shoe.name = "鞋子";
+      shoe.price = 300;
+
+      final Item sword = new Item();
+      sword.name = "长剑";
+      swrod.price = 350;
+      
+      System.out.println();
+    }
+}
